94d8291940547da0cc109308bd8313119f43ecbf
anhnguyenha09 <anhhnchinchin@gmail.com>
2025-08-09 19:45:26 +0700

    Commit

--- src/components/header.js
+++ src/components/header.js
@@ -1,3 +1,14 @@
-// function Header() {
-//     return ();
-// }
+function Header() {
+    return (
+        <header className="site-header">
+            <div className="container">
+                <h1 className="logo">My React Site</h1>
+                <nav className="nav">
+                    <a href="#home">Home</a>
+                    <a href="#about">About</a>
+                    <a href="#contact">Contact</a>
+                </nav>
+            </div>
+        </header>
+    )
+}
