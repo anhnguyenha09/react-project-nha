198153150082d67ee131dbeb9c15bcd31e662d2a
anhnguyenha09 <anhhnchinchin@gmail.com>
2025-08-16 19:35:07 +0700

    pretty

---
+++ src/components/Info.js
@@ -0,0 +1,15 @@
+// import LoginPage from "../containers/login";
+// import StudentList from "./StudentList";
+// import { Route, Routes } from "react-router-dom";
+function Info() {
+    return (
+        <p>Hello World</p>
+        // <main>
+        //     <Routes>
+        //         <Route path="/" element={<LoginPage />} />
+        //         <Route path="/studentlist" element={<StudentList />} />
+        //     </Routes>
+        // </main>
+    )
+}
+export default Info;
--- src/components/body.js
+++ src/components/body.js
@@ -1,36 +1,16 @@
-// ===== src/components/Body.js =====
-import React from 'react';
-
-export default function Body() {
-    //useState
-    const a = 3;
-    const fullName = 'HaAnh';
-    let age = 20;
-    let className = 'IT';
-    let school = 'NEU';
-
+import LoginPage from "../containers/Login";
+import StudentList from "./StudentList";
+import Info from "./Info";
+import { Route, Routes } from "react-router-dom";
+function Body() {
     return (
-        <div>
-            <h1>Họ và tên: ${fullName}</h1>
-            <h1>Tuổi: ${age}</h1>
-            <h1>Tên lớp: ${className}</h1>
-            <h1>Tên trường: ${school}</h1>
-        </div>
-        // <main className="site-body">
-        //     <div className="container">
-        //         <section className="hero">
-        //             <h2>Welcome to the simple layout</h2>
-        //             <p>This is a lightweight example using plain CSS and React components.</p>
-        //         </section>
-
-        //         <section className="content">
-        //             <h3>Content area</h3>
-        //             <p>
-        //                 Add your components, cards, lists, or whatever you need here. This
-        //                 area grows with content and will remain between the header and footer.
-        //             </p>
-        //         </section>
-        //     </div>
-        // </main>
-    );
+        <main>
+            <Routes>
+                <Route path="/" element={<LoginPage />} />
+                <Route path="/info" element={<Info />} />
+                <Route path="/studentlist" element={<StudentList />} />
+            </Routes>
+        </main>
+    )
 }
+export default Body;
