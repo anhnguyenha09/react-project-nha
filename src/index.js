2ec08c6a711ca74189036f7503b9f2dc1338fb0e
sonph2013 <sonph2013@gmail.com>
2025-08-29 19:47:56 +0700

    Anh sửa lại trong index.js mấy dòng import, em viết nhầm "header" thành "Header"

--- src/index.js
+++ src/index.js
@@ -1,9 +1,9 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
-import Header from './components/Header';
-import Body from './components/Body';
-import Footer from './components/Footer';
+import Header from './components/header';
+import Body from './components/body';
+import Footer from './components/footer';
 import reportWebVitals from './reportWebVitals';
 import { BrowserRouter } from 'react-router-dom';
 const root = ReactDOM.createRoot(document.getElementById('root'));
